a23b7f814e3007eb84cd46b1a24adb0a6559b5ae
john warui <kihatowarui@outlook.com>
2018-10-30 10:18:40 +0200

    Visual basic google search

---
+++ GoogleSearch/GoogleSearch/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim SearchName As String = TextBox1.Text
+        WebBrowser1.Navigate("https://www.google.com/search?q=" + SearchName)
+    End Sub
+End Class
